32c38bd722e6b0ac44ce1f659936951a2ac9a601
Cruz <cruzbarnum@gmail.com>
2020-03-03 00:29:22 -0800

    Made the project structure using Maven.

---
+++ ascii-render-lib/src/main/java/us/kosdt/arl/App.java
@@ -0,0 +1,13 @@
+package us.kosdt.arl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
